fec665719b2bab2e393c479a8c88eebdc403962b
Bryan Mullen <bryan@bryanmullen.me>
2021-09-14 04:29:35 +0000

    Restructure Profile To Render forms
    This commit updates on the previous profile form to correctly Render
    the default billing address. Javascript file to render the country
    correctly is currently broken

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-
 from .models import UserProfile
 
 
@@ -9,14 +8,18 @@ class UserProfileForm(forms.ModelForm):
         exclude = ('user',)
 
     def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
         super().__init__(*args, **kwargs)
         placeholders = {
             'default_phone_number': 'Phone Number',
+            'default_postcode': 'Postal Code',
+            'default_town_or_city': 'Town or City',
             'default_street_address1': 'Street Address 1',
             'default_street_address2': 'Street Address 2',
-            'default_town_or_city': 'Town or City',
-            'default_county': 'County',
-            'default_postcode': 'Post Code',
+            'default_county': 'County, State or Locality',
         }
 
         self.fields['default_phone_number'].widget.attrs['autofocus'] = True
@@ -27,6 +30,5 @@ class UserProfileForm(forms.ModelForm):
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'border-black' + \
-                'rounded-0 profile-form-input'
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
             self.fields[field].label = False
---
+++ static/js/displayCountry.js
@@ -0,0 +1,10 @@
+const countryField = document.getElementById('id_country') // used in checkout
+const defaultCountryField = document.getElementById('id_default_country') // used in profile
+
+if (countryField !== null){
+    countryField.style.display = "block"
+}
+
+if (defaultCountryField != null){
+    defaultCountryField.style.display = "block"
+}
