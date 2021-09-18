eae67f3a94d56646bb0bc068cd53593fe83c9753
Bryan Mullen <bryan@bryanmullen.me>
2021-09-18 21:53:55 +0000

    TEMPORARILY ADD DEBUG=TRUE

--- main/settings.py
+++ main/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
+# TODO: REMOVE THIS BEFORE SUBMISSION
 
 ALLOWED_HOSTS = ['bryansmullen-designr.herokuapp.com',
                  'localhost', '127.0.0.1']
