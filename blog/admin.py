bd5dd9649cf7b97c139462b7da6fc8d281bd86ba
Bryan Mullen <bryan@bryanmullen.me>
2021-09-18 15:22:32 +0000

    Edit Admin View of Blog To Display ID
    This commit manipulates the admin view of the Blog Model to show the
    ID field which is hidden by default as a ReadOnly field.

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import BlogEntry
 
-admin.site.register(BlogEntry)
+class BlogEntryAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+    fields = ('title','content','author','date_entered')
+    list_display = ('id', 'title','content','author','date_entered')
+    ordering = ('-date_entered',)
+
+admin.site.register(BlogEntry, BlogEntryAdmin)
