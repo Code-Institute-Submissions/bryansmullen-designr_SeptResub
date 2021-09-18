0d186e8e1519dc9711e1d11ba9a0c57d87885d9d
Bryan Mullen <bryan@bryanmullen.me>
2021-09-18 15:25:23 +0000

    Edit Admin View Of BlogEntry To Remove Content
    This commit removes 'content' from the admin view to prevent over
    bloating of this view

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,7 @@ from .models import BlogEntry
 class BlogEntryAdmin(admin.ModelAdmin):
     readonly_fields = ('id',)
     fields = ('title','content','author','date_entered')
-    list_display = ('id', 'title','content','author','date_entered')
+    list_display = ('id', 'title','author','date_entered')
     ordering = ('-date_entered',)
 
 admin.site.register(BlogEntry, BlogEntryAdmin)
